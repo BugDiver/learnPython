49c0465dfefc69033876ff1978426064e060e5ee
BugDiver <vinaysh@thoughtworks.com>
2016-03-03 17:43:58 +0530

    moved server to FLASK

--- server/simpleServer.py
+++ server/simpleServer.py
@@ -1,29 +1,41 @@
-from os import getcwd
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+# from os import getcwd
+# from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-PORT_NUMBER = 8080
+# PORT_NUMBER = 8080
 
-class myHandler(BaseHTTPRequestHandler):
-	
-	def do_GET(self):
-		if self.path=="/":
-			self.path="public/index.html"
-		else:
-			self.path = "public/"+self.path
+# class myHandler(BaseHTTPRequestHandler):
 
-		try:
-			file = open(getcwd() +'/'+self.path) 
-			self.send_response(200)
-			self.end_headers()
-			self.wfile.write(file.read())
-			file.close()
-			return
+# 	def do_GET(self):
+# 		if self.path=="/":
+# 			self.path="public/index.html"
+# 		else:
+# 			self.path = "public/"+self.path
 
-		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)
-	def log_message(self, format, *args):
-		return
+# 		try:
+# 			file = open(getcwd() +'/'+self.path)
+# 			self.send_response(200)
+# 			self.end_headers()
+# 			self.wfile.write(file.read())
+# 			file.close()
+# 			return
 
-server = HTTPServer(('', PORT_NUMBER), myHandler)
-server.serve_forever();
-print 'Started httpserver on port ' , PORT_NUMBER	
+# 		except IOError:
+# 			self.send_error(404,'File Not Found: %s' % self.path)
+# 	def log_message(self, format, *args):
+# 		return
+
+# server = HTTPServer(('', PORT_NUMBER), myHandler)
+# server.serve_forever();
+# print 'Started httpserver on port ' , PORT_NUMBER
+
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
