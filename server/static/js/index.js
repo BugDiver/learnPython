88bf3319a9034c2897c27b850703c5959eaae9f3
BugDiver <vinaysh@thoughtworks.com>
2016-03-03 18:45:23 +0530

    use jquery in sample ajax call

---
+++ server/static/js/index.js
@@ -0,0 +1,12 @@
+var postComment = function(){
+	$.post('comment',{comment : $('#comment').val()},function(response){
+		$('.input').html(response);
+	})
+}
+
+var onReady = function(){
+	$('#post').click(function(){
+		postComment();
+	})
+}
+$(document).ready(onReady);
